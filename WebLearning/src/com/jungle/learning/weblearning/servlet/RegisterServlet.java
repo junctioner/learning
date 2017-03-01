9ecf8b11cfdc3eaae46ab2df3a72fa21f2dc0866
baojunwen <baojunwen@yijiupi.com>
2017-03-01 13:13:09 +0800

    删除post方法中的注释

--- WebLearning/src/com/jungle/learning/weblearning/servlet/RegisterServlet.java
+++ WebLearning/src/com/jungle/learning/weblearning/servlet/RegisterServlet.java
@@ -21,7 +21,6 @@ public class RegisterServlet extends HttpServlet {
 	 */
     public RegisterServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -38,7 +37,6 @@ public class RegisterServlet extends HttpServlet {
 	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
