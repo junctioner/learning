3de0d2ea0be2b276e9dea8b9244cf665e6de933a
baojunwen <baojunwen@yijiupi.com>
2017-03-01 13:47:12 +0800

    添加注册参数处理

--- WebLearning/src/com/jungle/learning/weblearning/servlet/RegisterServlet.java
+++ WebLearning/src/com/jungle/learning/weblearning/servlet/RegisterServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.jungle.learning.weblearning.helper.RequestParams;
+
 /**
  * Servlet implementation class RegisterServlet
  */
@@ -28,7 +30,10 @@ public class RegisterServlet extends HttpServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		RequestParams rp = new RequestParams(request);
+		String name = rp.getString("name");
+		String pass = rp.getString("password");
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
